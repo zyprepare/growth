ac3182b886a7d61463e9ccdcc2f8bd81fab49238
zyprepare <zyprepare@163.com>
2018-03-28 15:56:53 +0800

    自动化配置入口文件

---
+++ tool/webpack/makeWebpack.js
@@ -0,0 +1,125 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const utils = require('./utils');
+
+// 是否是生产环境
+const production = process.env.NODE_ENV === 'production';
+// 根目录
+const ROOTDIR = process.cwd();
+const entry = utils.getEntry(path.resolve(process.cwd(), 'src/pages/'));
+const pages = utils.getPages(path.resolve(process.cwd(), 'src/pages/'));
+
+const makeWebpack = (options) => {
+  let config = {
+    entry: entry,
+    output: {
+      path: path.resolve(process.cwd(), 'dist'),
+      filename: '[name].js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader', //使用的加载器名称
+          query: { //babel的配置参数，可以写在.babelrc文件里也可以写在这里
+            presets: ['env', 'react', 'stage-1'],
+            plugins: ['transform-decorators-legacy', 'transform-decorators']
+          }
+        },
+        {
+          test: /\.css$/,
+          exclude: /node_modules/,
+          use: ExtractTextWebpackPlugin.extract({
+            fallback: "style-loader",
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  importLoaders: 1,
+                }
+              },
+              {
+                loader: 'postcss-loader',
+                options: {
+                  plugins: () => [
+                    require('autoprefixer'),
+                    require('precss'),
+                    require('postcss-flexbugs-fixes')
+                  ]
+                }
+              }
+            ]
+          }),
+        },
+        {
+          test: [/\.gif$/, /\.jpe?g$/, /\.png$/],
+          loader: 'url-loader',
+          options: {
+            limit: 10000, //1w字节以下大小的图片会自动转成base64
+          },
+        },
+        {
+          test: require.resolve('zepto'),
+          use: ['exports-loader?window.Zepto', 'script-loader']
+        }
+      ]
+    },
+    plugins: [
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor',
+        minChunks: 2
+      }),
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'manifest',
+        chunks: ['vendor'],
+        minChunks: Infinity,
+      }),
+      new ExtractTextWebpackPlugin("bundle.css?v=[chunkhash:8]"),
+      new webpack.BannerPlugin('垂直业务研发组'),
+    ].concat(pages),
+    resolve: {
+      extensions: ['.js', ".css", ".jsx"]
+    },
+  }
+
+  config.devtool = !production ? 'cheap-module-source-map' : 'null';
+  config.output.filename = !production ? '[name].js' : '[name].js?v=[chunkhash:8]';
+
+  if (!production) {
+    config.plugins.push(new webpack.HotModuleReplacementPlugin());
+    config.devServer = {
+      contentBase: path.resolve(process.cwd(), 'dist'),//本地服务器所加载的页面所在的目录
+      historyApiFallback: true,//不跳转
+      inline: true,//实时刷新
+      hot: true,
+      port: 8080,
+      proxy: {
+        '/api/*': {
+          target: 'http://localhost:9090/',
+          secure: false
+        }
+      }
+    }
+  } else {
+    config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }));
+    config.plugins.push(new CleanWebpackPlugin(
+      ['dist'], {
+        root: __dirname,
+        verbose: true,
+        dry: false
+      }
+    ));
+  }
+
+  return config;
+}
+
+module.exports = makeWebpack;
---
+++ tool/webpack/utils.js
@@ -0,0 +1,86 @@
+const fs = require('fs');
+const path = require('path');
+const join = path.join;
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+/**
+ * 获取打包入口文件列表
+ * @param {String} filePath
+ * @return {Object}
+ */
+const getEntry = (filePath) => {
+  return getNameAndFiles(filePath, 'jsx');
+}
+
+/**
+ * 获取HtmlWebpackPlugin列表
+ * @param {String} filePath
+ * @param {Array} chunks 默认打入vendor和manifes模块包
+ * @return {Object}
+ */
+const getPages = (filePath, chunks=['vendor', 'manifest']) => {
+  let obj = getNameAndFiles(filePath, 'html');
+  let arr = [];
+
+  for (let key in obj) {
+    arr.push(new HtmlWebpackPlugin({
+      template: obj[key],
+      filename: key + '.html',
+      chunks: [key].concat(chunks)
+    }));
+  }
+
+  return arr;
+}
+
+/**
+ * 获取文件名和文件路径
+ * @param {String} filePath
+ * @param {String} type 文件类型
+ * @return {Object}
+ */
+const getNameAndFiles = (filePath, type) => {
+  let result = {};
+  let reg = new RegExp(`\.${type}$`);
+
+  function find(fp) {
+    let files = fs.readdirSync(fp);
+
+    files.forEach(function (val, index) {
+      let fPath = join(fp, val);
+      let stats = fs.statSync(fPath);
+      let fileName = '';
+
+      if (stats.isDirectory()) {
+        find(fPath);
+      }
+
+      if (stats.isFile() && reg.test(fPath)) {
+        fileName = getFilename(fPath);
+        result[fileName] = fPath;
+      }
+    });
+  }
+
+  find(filePath);
+
+  return result;
+}
+
+/**
+ * 获取文件名规则：文件路径为src/pages/home/index.js，则文件名为home
+ * @param {String} filePath
+ * @return {String}
+ */
+const getFilename = (filePath) => {
+  let arr = filePath.split(path.sep);
+
+  return arr[arr.length - 2];
+}
+
+module.exports = {
+  getEntry: getEntry,
+  getPages: getPages,
+  getNameAndFiles: getNameAndFiles
+}
